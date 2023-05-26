1437ad34c17aebc5794825ac782aec4a3debd5e0
abuamar142 <amarjabon130@gmail.com>
2023-05-26 17:54:04 +0700

    add get movie by id

--- movie_api/views/movies.py
+++ movie_api/views/movies.py
@@ -1,18 +1,37 @@
-<<<<<<< HEAD
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from .. import db
 
 from .. models.movies import *
+
+from .. models.movies import Movie, Genre
+
+from .. middlewares.authentications import login_required, admin_only
 
 from .. utils import errors
 from uuid import uuid4
 
 mod_movie = Blueprint('mod_movie', __name__, url_prefix='/api/movies')
 
+@mod_movie.route('', methods=['GET'])
+@login_required
+def get_movies():
+    movies = Movie.to_list()
+    return {
+        'error': None,
+        'data': movies
+    }
 
-# @mod_movie.route('/<:id>', methods=['GET'])
-# def get_movie(id):
+@mod_movie.route('/<id>', methods=['GET'])
+@login_required
+def get_movie_byid(id):
+    movie = Movie.query.get(id)
+    if movie is None:
+        return errors.movie_not_found, 404
+    return movie.json, 200
+        
 @mod_movie.route('/delete/<:id>', methods=['DELETE'])
+@login_required
+@admin_only
 def delete_movie(id):
     movie = Movie.query.get(id)
     if movie is None:
@@ -26,32 +45,3 @@ def delete_movie(id):
             'id': id
         }
     }, 200
-=======
-from flask import Blueprint, request
-
-from .. import db 
-
-from .. models.movies import Movie, Genre
-
-from .. utils import errors
-
-from .. middlewares.authentications import login_required, admin_only
-
-mod_movie = Blueprint('mod_movie', __name__, url_prefix='/api/movies')
-
-@mod_movie.route('', methods=['GET'])
-@login_required
-def get_movies():
-    movies = Movie.to_list()
-    return {
-        'error': None,
-        'data': movies
-    }
-
-# @mod_movie.route('/<id>', methods=['GET'])
-# @login_required
-# def get_movie_byid(id):
-#     movie = Movie.query.get(id)
-#     if movie is None:
-        
->>>>>>> 0b66847d3082dd8a3b1a2634c7ffb52fe201c269
