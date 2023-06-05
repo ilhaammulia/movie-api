0f2cb26bafd837c08841e666008910eb571a6702
abuamar142 <amarjabon130@gmail.com>
2023-06-06 06:46:27 +0700

    update untuk genre agar bisa memasukkan list

--- movie_api/views/movies.py
+++ movie_api/views/movies.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Blueprint, request
 from .. import db
 
@@ -34,45 +35,44 @@ def get_movie_byid(id):
 def create_movie():
     id = str(uuid4())
     title = request.json.get('title')
-    release_date = request.json.get('release_date')
+
+    release_date_str = request.json.get('release_date')
+    release_date = datetime.strptime(release_date_str, "%Y-%m-%d").date()
+
     language = request.json.get('language')
     popularity = request.json.get('popularity')
     synopsis = request.json.get('synopsis')    
-    genres = request.json.get('genres')
+    genres = request.json.get('genres', [])
 
-    is_genres_exist = Genre.query.filter_by(name=genres).first()
+    genre_objects = []
 
-    if not is_genres_exist:
-        genre = request.json.get('genres')
-        genre = Genre(name=genre)
-        db.session.add(genre)
+    for genre_name in genres:
+        is_genres_exist = Genre.query.filter_by(name=genre_name).first()
 
-        print('Berhasil menambah genre')
-        
-        db.session.commit()
+        print(genre_name)
+        if not is_genres_exist:
+            genre = Genre(name=genre_name)
+            db.session.add(genre)
 
-    print(f'''
-            id = {id} dengan type data {type(id)}     
-            title = {title} dengan type data {type(title)}     
-            release_date = {release_date} dengan type data {type(release_date)}     
-            language = {language} dengan type data {type(language)}     
-            popularity = {popularity} dengan type data {type(popularity)}     
-            synopsis = {synopsis} dengan type data {type(synopsis)}     
-            genres = {genres} dengan type data {type(genres)}     
-    ''')
+            print('Berhasil menambah genre')
+            
+            db.session.commit()
+
+            genre_objects.append(genre)
+        else:
+            genre_objects.append(is_genres_exist)
 
     if not all([id, title, release_date, language, popularity, synopsis, genres]):
         return errors.bad_request_form, 400
     
     is_movie_exist = Movie.query.filter_by(title=title).first()
+
     if is_movie_exist:
         return errors.movie_exists, 200    
 
-    new_movie = Movie(id=id, title=title, release_date=release_date, language=language, popularity=popularity, synopsis=synopsis, genres=genres)
+    new_movie = Movie(id=id, title=title, release_date=release_date, language=language, popularity=popularity, synopsis=synopsis, genres=genre_objects)
 
-    print(new_movie.json)
-
-    db.session.add(new_movie.json)
+    db.session.add(new_movie)
     db.session.commit()
 
     return {
@@ -84,6 +84,7 @@ def create_movie():
                 "language":language,
                 "popularity":popularity,
                 "synopsis":synopsis,
+                "genres":genre_objects,
             }
         }, 201
 
